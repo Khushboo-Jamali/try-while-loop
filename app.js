ac0d0988394f23d61747d90ee9ec97ec951ceff7
khushboo-jamali <jamalikhushboo@gmail.com>
2024-05-20 13:44:13 +0100

    loops

---
+++ app.js
@@ -0,0 +1,7 @@
+let num = 25;
+let usernum = prompt("guess the user namber :");
+
+while (usernum != num) {
+  usernum = prompt("you enter wrong number guess again ");
+}
+console.log("welcome to our website");
